33352da4b8733ac65baeb53278ab85fe73bbee84
alex-njuguna <njugunakinuthia013@gmail.com>
2024-03-22 02:13:52 +0300

    view to create a display all books endpoint

--- library/api/views.py
+++ library/api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
 
-# Create your views here.
+from books.models import Book
+from .serializers import BookSerializer
+
+class BookAPIView(ListAPIView):
+    """display a collection of books from an api"""
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
