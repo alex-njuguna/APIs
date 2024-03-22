72d658e9d60428fac557e754a01fdd39b27ad0ad
alex-njuguna <njugunakinuthia013@gmail.com>
2024-03-23 00:13:31 +0300

    display a single todo details

--- todo/backend/todos/views.py
+++ todo/backend/todos/views.py
@@ -9,3 +9,8 @@ class ListTodo(ListAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
 
+class DetailTodo(RetrieveAPIView):
+    # display a single todo item
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+    
