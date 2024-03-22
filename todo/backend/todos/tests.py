7d7594e56d048bae79d18170240c1a9c0a90cf19
alex-njuguna <njugunakinuthia013@gmail.com>
2024-03-23 01:16:49 +0300

    tests for the Todo model

--- todo/backend/todos/tests.py
+++ todo/backend/todos/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Todo
+
+
+class TodoModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        # set up our data
+        Todo.objects.create(title='first todo', body='first todo body')
+
+    def test_title_content(self):
+        # test title content
+        todo = Todo.objects.get(id=1)
+        expected_object_name = f'{todo.title}'
+        self.assertEquals(expected_object_name, 'first todo')
+
+    def test_body_content(self):
+        # test body content is as expected
+        todo = Todo.objects.get(id=1)
+        expected_object_name = f'{todo.body}'
+        self.assertEquals(expected_object_name, 'first todo body')
