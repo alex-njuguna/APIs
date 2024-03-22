79ffe29768c0402a2c8591d4a827554e35fda2a7
alex-njuguna <njugunakinuthia013@gmail.com>
2024-03-22 23:44:13 +0300

    customization of the Todo model view in the admin pae

--- todo/backend/todos/admin.py
+++ todo/backend/todos/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Todo
+
+
+class TodoAdmin(admin.ModelAdmin):
+    # customize the Todo model viewing in the admin page
+    list_display = ['id', 'title', 'body']
+    list_display_links = ['id']
+    list_filter = ['title']
+    list_per_page = 25
+
+admin.site.register(Todo, TodoAdmin)
