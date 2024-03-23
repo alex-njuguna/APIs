7ff0f20cec1c7258c05a6050eec35a1ed73021a6
alex-njuguna <njugunakinuthia013@gmail.com>
2024-03-24 02:30:17 +0300

    view to cdisplay and create lists

--- blog/blogapi/posts/views.py
+++ blog/blogapi/posts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
-# Create your views here.
+from .models import Post
+from .serializers import PostSerializer
+
+class PostList(ListCreateAPIView):
+    # display a collectioon of posts as well as allow creating new ones
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
