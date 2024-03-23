0247875053304a778957c2ce785bf47f5219f64a
alex-njuguna <njugunakinuthia013@gmail.com>
2024-03-23 15:24:13 +0300

    fetch data from the django api with react

--- todo/frontend/src/App.jsx
+++ todo/frontend/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react"
+import axios from 'axios'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+
+ const [todos, setTodos] = useState([])
+
+const getTodos = () => {
+    axios
+      .get('http://localhost:8000/api/')
+      .then((res) => {
+        setTodos(res.data)
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
+  useEffect(() => {
+    getTodos()
+  }, [])
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>
+          <h1>{todo.title}</h1>
+          <p>{todo.body}</p>
+        </li>
+))}
+      </ul>
+      
+    </div>
   )
 }
-
-export default App
