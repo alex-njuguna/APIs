33d5911f544c083e7fad0595a40f88fcd93e737d
alex-njuguna <njugunakinuthia013@gmail.com>
2024-03-23 18:03:11 +0300

    customization of the view of the post model on admin page

--- blog/blogapi/posts/admin.py
+++ blog/blogapi/posts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    # customize post model view in the admin page
+    list_display = ['id', 'author', 'title', 'body', 'created_at', 'updated_at']
+    list_display_links = ['id', 'author', 'title']
+    list_filter = ['author']
+    list_per_page = 25
+
+admin.site.register(Post, PostAdmin)
